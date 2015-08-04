22718327513b39b63a8d5158e213d07646e3e075
cecyc <cecycorrea@gmail.com>
2015-08-04 17:26:42 -0500

    fixed seed file for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@ category_name = ['Soft', 'Semi-soft', 'Firm', 'Blue veined', 'Fresh']
 category_name.each do |name|
   Category.create(name: name)
 end
-
-Cheese.create(name: Faker::Hacker.adjective, thoughts: Faker::Lorem.paragraph, category_id: rand(1..5))
-Cheese.create(name: Faker::Hacker.adjective, thoughts: Faker::Lorem.paragraph, category_id: rand(1..5))
-Cheese.create(name: Faker::Hacker.adjective, thoughts: Faker::Lorem.paragraph, category_id: rand(1..5))
-Cheese.create(name: Faker::Hacker.adjective, thoughts: Faker::Lorem.paragraph, category_id: rand(1..5))
